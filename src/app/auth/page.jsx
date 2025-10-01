a6a74ed5ba2809aa7b01b30a74f69a3f225c142e
mintdexdev <dexdevpersona@gmail.com>
2025-10-01 07:00:00 +0530

    change files default function name

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+ 
+import TextField from "@/components/TextField";
 
 export default function Auth() {
   return (
-    <div>Auth</div>
+    <div>
+
+      <div className='flex h-screen'>
+        <div className='bg-gray-500 w-1/2'></div>
+
+        <div className='w-1/2 pr-[clamp(1rem,-0.3043rem+5.7971vw,5rem)]'>
+          <h1 className='text-l7'>Login</h1>
+          <p>Don't have an account? Create Account</p>
+
+
+          <TextField />
+          <TextField />
+ 
+          
+        </div>
+
+      </div>
+
+    </div>
   )
 }
---
+++ src/components/TextField.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function TextField(
+  {
+    htmlFor = 'input',
+    label = "label",
+    type = "text",
+  }
+) {
+  return (
+    <div className='relative'>
+      <label className='px-1 absolute transition-all -top-2.5 left-3.5 text-sm bg-c-1'
+
+        htmlFor={htmlFor}>{label}</label>
+      <input
+        className='w-full h-12 border rounded-lg px-4 text-lg focus-visible:outline-c-3 '
+        type={type}
+        id={htmlFor}
+        name=""
+      />
+    </div>
+  )
+}
