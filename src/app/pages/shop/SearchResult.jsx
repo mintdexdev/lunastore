70180675ae99e486e3ac3835701581eadaf8ba19
mintdexdev <dexdevpersona@gmail.com>
2025-10-07 11:34:00 +0530

    update search and help page

--- src/app/pages/shop/SearchResult.jsx
+++ src/app/pages/shop/SearchResult.jsx
@@ -1,7 +1,27 @@
+import { AllProducts, Container } from '@/components'
+
 import React from 'react'
 
 export default function SearchResult() {
   return (
-    <div>Search</div>
+    <div>
+      <div className={`relative aspect-[4/1]  grid place-items-center `}>
+
+        <div className='text-center '>
+          <p className='text-sm uppercase'>Search result for</p>
+          <h1 className='text-l7'>neon green tshirt</h1>
+        </div>
+
+      </div>
+
+      <section>
+        <Container>
+
+          <AllProducts />
+
+        </Container>
+      </section>
+
+    </div>
   )
 }
