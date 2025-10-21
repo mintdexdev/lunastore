a361adf2b9dc4b080b5d81236f75d654679716b7
mintdexdev <dexdevpersona@gmail.com>
2025-10-21 13:12:00 +0530

    update styling

--- src/app/pages/Account.jsx
+++ src/app/pages/Account.jsx
@@ -1,6 +1,7 @@
 import { Button, Container, OrderCard } from "@/components"
 import React from "react"
 
+const navList = ["Account Details", "Order History", "Billing and Payments", "Customer support", "Settings"]
 export default function Account() {
   return (
     <section>
@@ -8,7 +9,7 @@ export default function Account() {
 
         <div className="w-4/12">
 
-          <div className="grid justify-center text-center">
+          <div className="mb-8 grid justify-center text-center">
             <div className="rounded-full overflow-clip w-50 aspect-square">
               <img src="/.temp/images/category.jpg" alt="" />
             </div>
@@ -17,29 +18,29 @@ export default function Account() {
           </div>
 
           <nav>
-            <ul className="grid gap-4">
-              <li className="bg-helper5 rounded-lg px-4 py-2">Personal Information</li>
-              <li className="bg-helper5 rounded-lg px-4 py-2">Order History</li>
-              <li className="bg-helper5 rounded-lg px-4 py-2">My Addresses</li>
-              <li className="bg-helper5 rounded-lg px-4 py-2">Billing and Payments</li>
-              <li className="bg-helper5 rounded-lg px-4 py-2">Customer support</li>
+            <ul className="grid">
+              {navList.map(item => (
+                <li className="rounded-xl px-[13px] py-2.5 hover-bg-helper5"
+                  key={item}
+                >{item}
+                </li>
+              ))}
             </ul>
           </nav>
 
-          <Button className="bg-c-4 w-fit mt-6 mx-auto">Logout</Button>
         </div>
 
         <div className="w-8/12">
-          <h2 className="text-l4 text-center">Order History</h2>
-          <div className="grid gap-4">
-            <OrderCard />
-            <OrderCard />
-            <OrderCard />
+          <h1 className="text-l4 mb-4 text-center">Order History</h1>
+          <div className="grid">
+            <div className="grid gap-4 border p-4 rounded-4xl">
+              <OrderCard />
+              <OrderCard />
+              <OrderCard />
+            </div>
           </div>
         </div>
-
       </Container>
     </section>
-
   )
 }
