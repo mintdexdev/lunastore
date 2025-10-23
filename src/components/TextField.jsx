077d23c688a2fa4954d9f2a15f5740eadb39e442
mintdexdev <dexdevpersona@gmail.com>
2025-10-23 14:12:00 +0530

    rounded change

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -15,7 +15,7 @@ export default function TextField({
       <label className={`px-1 absolute left-3.5 -top-2.5 text-sm bg-c-1 [&:has(+input:focus)]:text-c-3`}
         htmlFor={id}>{label}</label>
       <input
-        className={twMerge("bg-c-1 w-full h-12 rounded-lg  border px-4 text-lg focus:outline-c-3", className)}
+        className={twMerge("bg-c-1 w-full h-12 rounded-xl  border px-4 text-lg focus:outline-c-3", className)}
         type={type}
         id={id}
         name={id}
