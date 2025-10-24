47228684f168ac28d440bf8b2570cf37b9d6b0a9
mintdexdev <dexdevpersona@gmail.com>
2025-10-24 14:12:00 +0530

    full screen fix

--- src/app/pages/Auth.jsx
+++ src/app/pages/Auth.jsx
@@ -3,17 +3,17 @@ import SignInUp from "@/components/SignInUp";
 import { Link, useLocation } from "react-router";
 
 export default function Auth() {
- 
-  return (
-    <div className="flex h-screen">
 
-      <div className="w-1/3 h-full bg-gray-500 ">
+  return (
+    <div className="flex min-h-screen ">
+
+      <div className="w-1/2  bg-gray-500">
         {/* <img src="" alt="" /> */}
       </div>
 
-      <div className="w-2/3 overflow-y-scroll relatives">
-        <Link className="absolute right-8 top-4" 
-        to={{ pathname: '/shop' }}>X</Link>
+      <div className="w-1/2 relatives p-6">
+        <Link className="absolute right-8 top-4"
+          to={{ pathname: '/shop' }}>X</Link>
         <SignInUp />
       </div>
     </div >
