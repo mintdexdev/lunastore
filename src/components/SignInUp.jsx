5b7d2c9e912922616e9d6dcc2f9298b308afd6c4
mintdexdev <dexdevpersona@gmail.com>
2025-10-27 14:12:00 +0530

    consistent ui

--- src/components/SignInUp.jsx
+++ src/components/SignInUp.jsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
-import { Link } from "react-router";
+import { useState } from 'react';
+import { Link } from 'react-router';
 
-import { AUTH_CONSTANTS } from "@/app/constants/auth.constants";
+import { AUTH_CONSTANTS } from '@/app/constants/auth.constants';
 import { Button, Container, Divider, SplitButton, TextField } from '@/components'
 
+const socialWith = ["Google", "Facebook", "X"]
 
 export default function SignInUp() {
   const [currentAuth, setcurrentAuth] = useState(AUTH_CONSTANTS.SIGN_IN);
   return (
-    <Container variant="form" className="py-6 max-w-[640px]">
+    <Container variant='form' className='py-6 max-w-[640px]'>
       {currentAuth === AUTH_CONSTANTS.SIGN_IN ?
         <SignIn setcurrentAuth={setcurrentAuth} /> :
         <SignUp setcurrentAuth={setcurrentAuth} />
@@ -20,49 +21,48 @@ export default function SignInUp() {
 function SignUp({ setcurrentAuth }) {
   return (
     <>
-      <h1 className="text-l7">Create Account</h1>
+      <h1 className='text-l6'>Create Account</h1>
 
-      <form action="">
-        <div className="grid gap-4">
-          <p>already have an account? <span className="underline cursor-pointer" onClick={() => setcurrentAuth(AUTH_CONSTANTS.SIGN_IN)}>login</span></p>
-          <div className="flex gap-4">
-            <TextField label="First Name" type="text" id="firstName" />
-            <TextField label="Last Name" type="text" id="lastName" />
+      <form action=''>
+        <div className='grid gap-8'>
+          <div className='flex gap-4'>
+            <TextField label='First Name' type='text' id='firstName' />
+            <TextField label='Last Name' type='text' id='lastName' />
           </div>
-          <TextField label="Email Address" type="email" id="emailAddress" />
-          <TextField label="Password" type="password" id="pasword" />
-          <SplitButton inputType="number" label="Verification Code">
+          <TextField label='Email Address' type='email' id='emailAddress' />
+          <TextField label='Password' type='password' id='pasword' />
+          <SplitButton inputType='number' label='Verification Code'>
             Send Verification Code
           </SplitButton >
         </div>
 
-        <div className="grid gap-2 mt-8 text-sm">
+        <div className='grid gap-2 mt-8 mb-2 text-sm'>
           <p>luna may keep me informed with personalized email about products and services.<br />
-            See our <span className="underline"><Link to={{ pathname: "/privacy" }}>Privacy Policy</Link></span>for more details</p>
-
+            See our <span className='underline'><Link to={{ pathname: '/privacy' }}>Privacy Policy</Link></span> for more details.</p>
           <div>
-            <input type="checkbox" name="" id="contact" /> <label htmlFor="">Please contact me via e-mail</label>
+            <input type='checkbox' name='' id='contact' /> <label htmlFor=''>Please contact me via e-mail</label>
           </div>
           <div>
-            <input type="checkbox" name="" id="accept-terms" /> <label htmlFor="">I have read and accept the terms and Conditions</label>
+            <input type='checkbox' name='' id='accept-terms' /> <label htmlFor=''>I have read and accept the terms and Conditions</label>
           </div>
         </div>
 
-        <Button className="my-8">Create Account</Button>
+        <Button>Create Account</Button>
       </form>
+
+      <p className='mt-2 mb-8'>Already have an account? <span className='underline cursor-pointer' onClick={() => setcurrentAuth(AUTH_CONSTANTS.SIGN_IN)}>Login</span></p>
 
       <Divider />
 
-      <p className="text-center  mt-8 ">or register with</p>
+      <p className='text-center mt-4 '>or register with</p>
 
-      <div className="flex gap-4 mt-4 mx-auto w-fit">
-        <div className="flex gap-2 items-center px-4 py-2 border rounded-lg"><div className="bg-gray-500 w-4 h-4 rounded-full" /> Google</div>
-        <div className="flex gap-2 items-center px-4 py-2 border rounded-lg"><div className="bg-gray-500 w-4 h-4 rounded-full" /> Facebook</div>
-        <div className="flex gap-2 items-center px-4 py-2 border rounded-lg"><div className="bg-gray-500 w-4 h-4 rounded-full" /> X</div>
-
+      <div className='flex gap-4 mt-4 mx-auto w-fit'>
+        {socialWith.map(item => (
+          <div className='flex gap-2 items-center px-4 py-2 border rounded-xl'><div className='bg-gray-500 w-4 h-4 rounded-full'
+            key={item}
+          /> {item}</div>
+        ))}
       </div>
-
-
     </>
   )
 }
@@ -70,35 +70,32 @@ function SignUp({ setcurrentAuth }) {
 function SignIn({ setcurrentAuth }) {
   return (
     <>
-      <h1 className="text-l7">Login to Account</h1>
+      <h1 className='text-l6'>Login to Account</h1>
 
-      <form action="" >
-        <div className="grid gap-4">
-          <p>don't have an account? <span className="underline cursor-pointer" onClick={() => setcurrentAuth(AUTH_CONSTANTS.SIGN_UP)}>create account</span></p>
-          <TextField label="Email Address" type="email" id="emailAddress" />
-          <TextField label="Password" type="password" id="pasword" />
+      <form>
+        <div className='grid gap-8'>
+          <TextField label='Email Address' type='email' id='emailAddress' />
+          <TextField label='Password' type='password' id='pasword' />
+          <div className='flex gap-2'>
+            <input type='checkbox' name='' id='accept-terms' />
+            <label htmlFor=''>Keep me logged in</label>
+          </div>
+          <Button className=''>Login</Button>
         </div>
-
-        <div className=" mt-4">
-
-
-          <input type="checkbox" name="" id="accept-terms" /> <label htmlFor="">Keep me logged in</label>
-
-        </div>
-
-        <Button className="my-8">Create Account</Button>
       </form>
+      <p className='mt-2 mb-8'>Don't have an account? <span className='underline cursor-pointer' onClick={() => setcurrentAuth(AUTH_CONSTANTS.SIGN_UP)}>Create Account</span></p>
 
       <Divider />
 
-      <p className="text-center  mt-8 ">or login with</p>
+      <p className='text-center mt-4 '>or login with</p>
 
-      <div className="flex gap-4 mt-4 mx-auto w-fit">
-        <div className="flex gap-2 items-center px-4 py-2 border rounded-lg"><div className="bg-gray-500 w-4 h-4 rounded-full" /> Google</div>
-        <div className="flex gap-2 items-center px-4 py-2 border rounded-lg"><div className="bg-gray-500 w-4 h-4 rounded-full" /> Facebook</div>
-        <div className="flex gap-2 items-center px-4 py-2 border rounded-lg"><div className="bg-gray-500 w-4 h-4 rounded-full" /> X</div>
+      <div className='flex gap-4 mt-4 mx-auto w-fit'>
+        {socialWith.map(item => (
+          <div className='flex gap-2 items-center px-4 py-2 border rounded-xl'><div className='bg-gray-500 w-4 h-4 rounded-full'
+            key={item}
+          /> {item}</div>
+        ))}
       </div>
-
     </>
   )
 }
